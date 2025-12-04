1b26d2847c0c4ecf7e0a3f9513b59d3d2ef9d891
NAOKITORIUMI <naokii.n.music@gmail.com>
2025-12-04 15:38:07 +0900

    tailwind実装

--- src/sample/config/settings.py
+++ src/sample/config/settings.py
@@ -41,7 +41,15 @@ INSTALLED_APPS = [
     'users',
     'books',
     'core',
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+if DEBUG:
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -60,7 +68,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
